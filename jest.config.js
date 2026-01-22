41bb25a987ec0a3d4d56438e4fd2ba0c13740a3f
Thaha CHAHBI <84104221+t-chahbi@users.noreply.github.com>
2026-01-22 10:15:00 +0100

    chore(deps): configure Jest with @react-native/jest-preset and fix babel config typo

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
   transformIgnorePatterns: [
